3a7ae5e6743eec50192448da7e1d022e7a1558b9
yougmi <zoz7353@gmail.com>
2020-07-02 10:38:26 +0900

    edit git ignore

---
+++ build/precache-manifest.54dd8ddfcd5bd1421ca1dfaf4ad8ec1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5d204f04690d2cf9d8e04add6cfc79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bf6abdf058af9187a4",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "bad562fd297350ee4dde",
+    "url": "/static/css/main.95b2126b.chunk.css"
+  },
+  {
+    "revision": "76bf6abdf058af9187a4",
+    "url": "/static/js/2.859cdca8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.859cdca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad562fd297350ee4dde",
+    "url": "/static/js/main.92f14b27.chunk.js"
+  },
+  {
+    "revision": "fc33ad2c9c863f2beddc",
+    "url": "/static/js/runtime-main.a7e357f1.js"
+  }
+]);
