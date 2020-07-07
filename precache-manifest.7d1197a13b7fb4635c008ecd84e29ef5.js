93fcb102ec40d1c4c6291eb79a6809efcc56dc7c
yougmi <zoz7353@gmail.com>
2020-07-07 11:26:03 +0900

    Updates

---
+++ precache-manifest.7d1197a13b7fb4635c008ecd84e29ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5100f68f2a19b41f0016b7e90c2cc55",
+    "url": "/movie_react_study/index.html"
+  },
+  {
+    "revision": "3a22bda8190da47d49a6",
+    "url": "/movie_react_study/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "0188b545505d3254b32d",
+    "url": "/movie_react_study/static/css/main.c12ea830.chunk.css"
+  },
+  {
+    "revision": "3a22bda8190da47d49a6",
+    "url": "/movie_react_study/static/js/2.152ee229.chunk.js"
+  },
+  {
+    "revision": "d122b28c0242baea262f6b8590ca6d94",
+    "url": "/movie_react_study/static/js/2.152ee229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0188b545505d3254b32d",
+    "url": "/movie_react_study/static/js/main.75625b1b.chunk.js"
+  },
+  {
+    "revision": "73113922b3f82b9f8bf0",
+    "url": "/movie_react_study/static/js/runtime-main.f9606137.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_react_study/precache-manifest.0a39c6d74166f8ff7083017156da5a80.js"
+  "/movie_react_study/precache-manifest.7d1197a13b7fb4635c008ecd84e29ef5.js"
 );
 
 self.addEventListener('message', (event) => {
